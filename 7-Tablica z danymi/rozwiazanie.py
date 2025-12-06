e2d43d2378094f4f66c213f2cca85709d7bbc2bd
MaksMil <maks.milcuszek@gmail.com>
2025-12-06 11:38:51 +0100

    Małe litery i spacje
    🤤

--- 7-Tablica z danymi/rozwiazanie.py
+++ 7-Tablica z danymi/rozwiazanie.py
@@ -1,13 +1,16 @@
-from api import tabela
-def decode_hidden_name(tabela):
+from tabela import table
+def decode_hidden_name(table):
     letters = []
 
-    for i, num in enumerate(tabela):
+    for i, num in enumerate(table):
         if i % 21 == 19:
             last = num % 100
             if 1 <= last <= 26:
                 letters.append(chr(last + 64))
-
+            elif 27 <= last <= 52:       # a–z
+                letters.append(chr(last - 27 + 97))
+            elif last == 0 or last == 53:  # spacja
+                letters.append(" ")
 
     return "".join(letters)
 
