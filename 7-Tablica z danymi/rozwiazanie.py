e42926100c207bc970f775418f900fd58c0e0c96
MaksMil <maks.milcuszek@gmail.com>
2025-12-06 11:02:53 +0100

    ups
    whops

--- 7-Tablica z danymi/rozwiazanie.py
+++ 7-Tablica z danymi/rozwiazanie.py
@@ -1,11 +1,12 @@
-from api import data_table
+from api import table
 def decode_hidden_name(table):
     letters = []
 
-    for i, num in enumerate(data_table):
+    for i, num in enumerate(table):
         if i % 21 == 19:
             last = num % 100
             if 1 <= last <= 26:
                 letters.append(chr(last + 64))
+
 
     return "".join(letters)
